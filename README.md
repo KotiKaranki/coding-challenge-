03dadd9122d8668f5435ff34462f585f6579dd7e
unknown <koteswara.r.karanki@gmail.com>
2019-10-17 04:33:02 +0530

    Spring boot maven facet added to the project

---
+++ README.md
@@ -0,0 +1,1 @@
+# coding-challenge-banking-system
---
+++ src/main/java/org/karanki/coding/challenge/banking/CodingChallengeBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.karanki.coding.challenge.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeBankingSystemApplication.class, args);
+	}
+
+}
